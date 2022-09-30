b6571454d76f2486de0f162bb04dbdff303d8cf9
Kain Fabian <gamesmaster3000x@gmail.com>
2022-09-30 20:22:59 +0100

    Can display hex dump of files.

--- RF_ASM/Program.cs
+++ RF_ASM/Program.cs
@@ -1,11 +1,11 @@
-﻿class Program
-{
-    static void Main(string[] args)
+﻿namespace RF_ASM {
+    class Program
     {
-        Console.Write("Enter the name of the file to be read (including the file extension): ");
-        string s = Console.ReadLine();
-        BinaryReader reader = new BinaryReader(File.Open("resources/" + s, FileMode.OpenOrCreate));
-        Console.WriteLine(Convert.ToHexString(new byte[]{reader.ReadByte()}));
-        reader.Close();
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the name of the file to be read (including the file extension): ");
+            byte[] data = Utils.GetDataFromFile("resources/" + Console.ReadLine());
+            Utils.DisplayHexDump(data);
+        }
     }
 }
---
+++ RF_ASM/Utils.cs
@@ -0,0 +1,37 @@
+﻿namespace RF_ASM
+{
+    internal class Utils
+    {
+
+        public static byte[] GetDataFromFile(string path)
+        {
+            BinaryReader reader = new BinaryReader(File.Open(path, FileMode.OpenOrCreate));
+            byte[] data = new byte[reader.BaseStream.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = reader.ReadByte();
+            }
+            reader.Close();
+            return data;
+        }
+
+        public static void DisplayHexDump(byte[] data)
+        {
+            int lines = (data.Length + 15) / 16;
+            string linesHex = (lines - 1).ToString("X");
+            Console.WriteLine(new string(' ', linesHex.Length) + " 00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F");
+            for (int i = 0; i < lines; i++)
+            {
+                Console.Write(i.ToString("X" + linesHex.Length.ToString()));
+                for (int j = 0; j < 16; j++)
+                {
+                    if (i * 16 + j < data.Length)
+                    {
+                        Console.Write(" " + data[i * 16 + j].ToString("X2"));
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
