780336ca9707c9c10de9b04cacccf73336dbb9c3
gamesmaster3000x <gamesmaster3000x@gmail.com>
2022-10-01 12:50:05 +0100

    Added stack class and

---
+++ RF_ASM/Computer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RF_ASM
+{
+    internal class Computer
+    {
+        public byte[] memory;
+
+        //Must be at least 3 elements, which are registers A, B and C.
+        public byte[] registers;
+
+        public Stack stack;
+    }
+}
--- RF_ASM/Program.cs
+++ RF_ASM/Program.cs
@@ -8,11 +8,12 @@
         static void Main(string[] args)
         {
             Console.Write("Enter the name of the file to be read (including the file extension): ");        
-            string filePath = Console.ReadLine();
             
             // The raw byte data from the given file
-            byte[] data = Utils.GetDataFromFile("resources/" + filePath);
+            byte[] data = Utils.GetDataFromFile("resources/" + Console.ReadLine());
             Utils.DisplayHexDump(data);
+
+
         }
     }
 }
