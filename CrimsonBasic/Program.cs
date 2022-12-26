392469e49bd840598a266356fdc8a3aed34dd6d1
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2022-12-26 14:52:30 +0000

    Created CrimsonBasic

---
+++ CrimsonBasic/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CrimsonBasic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
