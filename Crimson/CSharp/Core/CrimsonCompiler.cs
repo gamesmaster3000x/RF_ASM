f75b22aada4024d2d536ddff3f768032161d03b7
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2022-12-26 14:07:52 +0000

    Proposed CrimsonRFP

--- Crimson/CSharp/Core/CrimsonCompiler.cs
+++ Crimson/CSharp/Core/CrimsonCompiler.cs
@@ -24,9 +24,45 @@ namespace Crimson.CSharp.Core
 
         public int FullyCompileFromOptions()
         {
+            // Get the root unit (ie. main.crm)
             CompilationUnit rootUnit = UnitGenerator.GetUnitFromPath(Options.CompilationSourcePath);
+
+            // Generate dependency units (all resources are now accessible)
             Compilation compilation = new Compilation(rootUnit, UnitGenerator);
+
+            // Link FunctionCalls
             LinkedUnit linkedUnit = Linker.Link(compilation);
+
+            /*
+             * Now we need to break this list into simple statements - a strange kind of Crimson/RFASM mash-up.
+             * This strange intermediate language will have some high-level features from Crimson, but will use a flattened control flow.
+             * This basically means that conditions will be replaced with jumps.
+             * 
+             * >> main.crm
+             *  function main () {
+             *      int i = 5;
+             *      
+             *      if (i == 4) {
+             *          i = 6;
+             *      } else {
+             *          return false;
+             *      }
+             *  }
+             * 
+             * >> main.crmrfp
+             *  ::func_main
+             *  int i = 5
+             *  bool b = (i == 4)
+             *  
+             *  JNE b, true, "not_equal"        // Jump if condition false (ie. i != 4)
+             *      i = 6
+             *      JMP "end_condition"
+             *  ::not_equal                     // This is the else block
+             *      return false;
+             *  ::end_condition
+             *  ::endfunc_main
+             */
+
             return 1;
         }
 
