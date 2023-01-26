7b2ad96be11a664988ccb6910c585ee5bbc504ca
Kain Fabian <gamesmaster3000x@gmail.com>
2023-01-26 11:59:32 +0000

    Fully implemented all processor instructions.

---
+++ RedFoxVM/Components/Stack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedFoxVM.Components
+{
+    internal class Stack
+    {
+        private Word[] data;
+        private int pointer = 0;
+
+        public Stack(int size)
+        {
+            data = new Word[size];
+            for (int i = 0; i < size; i++)
+            {
+                data[i] = new Word(Computer.DataWidth);
+            }
+        }
+
+        public void Push(Word word)
+        {
+            data[pointer++] = new Word((word).Bytes);
+        }
+
+        public Word Pop()
+        {
+            return new Word(data[--pointer].Bytes);
+        }
+
+        public Word Peek()
+        {
+            return new Word(data[pointer - 1].Bytes);
+        }
+    }
+}
