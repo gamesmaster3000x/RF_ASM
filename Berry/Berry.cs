dd7b1482f4a5d5be730914617b6c11fa1f842702
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2023-04-16 21:51:59 +0100

    Can now deploy/publish Berry locally
    Just to check it's working

--- Berry/Berry.cs
+++ Berry/Berry.cs
@@ -4,29 +4,54 @@ namespace Berry
     {
         public static int Main (string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            Run();
+            return 0;
+        }
+
+        public static void Run ()
+        {
+            WebApplicationBuilder builder = WebApplication.CreateBuilder();
+
+            _ = builder.WebHost.ConfigureKestrel(serverOptions =>
+            {
+                serverOptions.ConfigureEndpointDefaults(listenOptions =>
+                {
+
+                });
+
+                serverOptions.ConfigureHttpsDefaults(listenOptions => { });
+            });
+
+
 
             // Add services to the container.
-            builder.Services.AddRazorPages();
+            _ = builder.Services.AddRazorPages();
 
             var app = builder.Build();
+
+            string expression = @".+exp.+";
+            _ = app.MapGet("/get/{id:required:regex(" + expression + ")}", (string id) => Get(id));
+
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Error");
+                _ = app.UseExceptionHandler("/Error");
             }
-            app.UseStaticFiles();
+            _ = app.UseStaticFiles();
 
-            app.UseRouting();
+            _ = app.UseRouting();
 
-            app.UseAuthorization();
+            _ = app.UseAuthorization();
 
-            app.MapRazorPages();
+            _ = app.MapRazorPages();
 
             app.Run();
+        }
 
-            return 0;
+        public static string Get (string id)
+        {
+            return $"Hello, World! {id}";
         }
     }
 }
