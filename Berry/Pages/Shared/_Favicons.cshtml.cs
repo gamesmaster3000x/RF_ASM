ec1200da30eb60874197f762235e948b878dc32a
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2023-04-18 20:41:12 +0100

    Created Berry initial logo and favicons
    - Courtesy of https://realfavicongenerator.net/ (pretty neat website)

---
+++ Berry/Pages/Shared/_Favicons.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Berry.Pages.Shared
+{
+    public class _FaviconsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Berry/src/API/IBerryClient.cs
+++ Berry/src/API/IBerryClient.cs
@@ -1,4 +1,4 @@
-﻿namespace Berry.API
+﻿namespace Berry.src.API
 {
     public interface IBerryClient
     {
