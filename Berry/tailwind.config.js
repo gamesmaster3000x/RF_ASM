6a587f0f52ed20e15a741d5e8895b5baf683af8d
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2023-04-19 20:30:39 +0100

    Colour -> Color :/ because Americanisms

--- Berry/tailwind.config.js
+++ Berry/tailwind.config.js
@@ -10,19 +10,19 @@ module.exports = {
     theme: {
         extend: {},
 
-        colours: {
+        colors: {
             transparent: 'transparent',
-            current: 'currentColor',,
-            'light-magenta': '#92374d',
-            'light-mint': '#cbf3f0',
-            'light-white': '#e9e8ed',
-            'light-tangerine': '#f68f5e',
-            'light-bittersweet': '#f76d5f',
-            'dark-majorelle': '#574AE2',
-            'dark-royal': '#222A68',
-            'dark-hunter': '#4B644A',
-            'dark-ash': '#c7d6d5',
-            'dark-quartz': '#9C92A3',
+            current: 'currentColor',
+            'magenta': '#92374d',
+            'mint': '#cbf3f0',
+            'white': '#e9e8ed',
+            'tangerine': '#f68f5e',
+            'bittersweet': '#f76d5f',
+            'majorelle': '#574AE2',
+            'royal': '#222A68',
+            'hunter': '#4B644A',
+            'ash': '#c7d6d5',
+            'quartz': '#9C92A3',
         }
     },
     variants: {
