bc206635e30822deee2a12c94cf89397d09bf794
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2023-05-07 20:05:49 +0100

    Created Berry structures in compiler
    + Berry
    + FunctionEntry
    + FunctionEntry.Converter
    + GlobalVariableEntry
    > Moved Compiler readme.md to Compiler/API/Berry because it only talked about Berries anyway

---
+++ Crimson/Compiler/API/Berry/FunctionEntry.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Compiler.API.Berry
+{
+    [JsonConverter(typeof(Converter))]
+    public class FunctionEntry
+    {
+
+        public byte[] MachineCode { get; set; } = new byte[0];
+        public string Source { get; set; } = "";
+        public string Links { get; set; } = "";
+
+        public FunctionEntry () { }
+
+        public FunctionEntry (byte[] machineCode, string source, string links)
+        {
+            MachineCode = machineCode;
+            Source = source;
+            Links = links;
+        }
+
+        /// <summary>
+        /// Custom 
+        /// </summary>
+        public class Converter : JsonConverter<FunctionEntry>
+        {
+            public override FunctionEntry? Read (ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            {
+                if (reader.TokenType != JsonTokenType.StartObject)
+                    throw new JsonException("Berry function entries must be JSON objects. Found: " + reader.TokenType + " at " + reader.Position);
+
+                string mcb64 = GetJsonString("mcb64", reader);
+                string srcb64 = GetJsonString("srcb64", reader);
+                string links = GetJsonString("links", reader);
+
+                byte[] mc = Convert.FromBase64String(mcb64);
+                byte[] srcBytes = Convert.FromBase64String(srcb64);
+                string src = Berry.ENCODING.GetString(srcBytes);
+
+                return new FunctionEntry(mc, src, links);
+            }
+
+            private string GetJsonString (string key, Utf8JsonReader reader)
+            {
+                // Key
+                if (reader.TokenType != JsonTokenType.PropertyName)
+                    throw new JsonException(
+                        $"Illegal Berry function entry syntax. " +
+                        $"Expected property name, got '{reader.TokenType}' at position {reader.Position}");
+
+                string readKey = reader.GetString()!;
+                if (!key.Equals(readKey))
+                    throw new JsonException(
+                        $"Illegal Berry function entry syntax. " +
+                        $"Expected property '{key}', got '{readKey}' at position {reader.Position}");
+
+                // Value
+                if (!reader.Read())
+                    throw new JsonException($"Reader expected a value, but was unable to read past position {reader.Position}.");
+
+                if (reader.TokenType != JsonTokenType.String)
+                    throw new JsonException(
+                       $"Illegal Berry function entry syntax. " +
+                       $"Expected property '{key}', got '{readKey}' at position {reader.Position}");
+
+                string value = reader.GetString()!;
+                return value;
+            }
+
+            public override void Write (Utf8JsonWriter writer, FunctionEntry value, JsonSerializerOptions options)
+            {
+                string mcb64 = Convert.ToBase64String(value.MachineCode);
+                byte[] srcBytes = Berry.ENCODING.GetBytes(value.Source);
+                string srcb64 = Convert.ToBase64String(srcBytes);
+
+                writer.WriteString("mcb64", mcb64);
+                writer.WriteString("srcb64", srcb64);
+                writer.WriteString("links", value.Links);
+            }
+        }
+    }
+}
---
+++ Crimson/Compiler/API/Berry/GlobalVariableEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Compiler.API.Berry
+{
+    [JsonConverter(typeof(Converter))]
+    public class GlobalVariableEntry
+    {
+        [JsonPropertyName("size")] public int SizeBytes { get; set; }
+    }
+}
